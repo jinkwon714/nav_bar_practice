afaa73a8cbcf23e9e5e21291086d7de7394f5340
Hyukjin Kwon <hjnim0909@gmail.com>
2020-12-06 12:05:06 +0900

    added remarks
    added remarks

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// using querySelector for active function
 const toggleBtn = document.querySelector('.navbar__toggleBtn');
 const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
